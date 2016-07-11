a24c6888a488cda0b312360402aa9a5336322290
Maxim <m.v.korotenko@gmail.com>
2016-07-11 09:34:36 +0300

    app

--- js/router.js
+++ js/router.js
@@ -1,31 +1,15 @@
 // Filename: router.js
 define([
-  'jquery',
-  'dc/backbone',
-  'dc/app'
+  'jquery', 'backbone', 'app'
 ], function($, Backbone, app){
 
     "use strict";
 
     var AppRouter = Backbone.Router.extend({
       routes: {
-        '' : 'nodePage',
-        'level/:level/node/:node': 'nodePage',
-        'level/:level/node/:node/:measure': 'unitAnalyticsPage',
-        '*pageNotFound': 'pageNotFound'
+        '' : 'mainpage'
       },
-      nodePage: function (level, node) {
-          if (!level && !node) {
-              level = 'A';
-              node = 'Global';
-          };
-          app.showPage(level, node);
-      },
-      unitAnalyticsPage: function (level, node, measure) {
-          app.showPage(level, node, measure);
-      },
-      pageNotFound: function () {
-          app.showPage404();
+      mainpage: function () {
       },
       initiliaze: function () {}
     });
